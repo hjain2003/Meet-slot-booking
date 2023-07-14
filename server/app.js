a21ddfa470907fe42ccb71dd58a6aba1f9a4f7ce
Harsh <hjain_be21@thapar.edu>
2023-07-15 01:57:31 +0530

    .

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ connectDB();
 //middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '',
     credentials: true,
   }));
 app.use(express.json());
