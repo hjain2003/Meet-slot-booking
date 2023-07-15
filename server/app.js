ea69ea0de1acc0256dd1e1cf65be9caecf851137
Harsh <hjain_be21@thapar.edu>
2023-07-15 10:13:42 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ import cors from 'cors';
 import { connectDB } from './dbconn/conn.js';
 import MeetRouter from './routes/meet_routes.js';
 
-const app=express();
-dotenv.config({path:'./config.env'});
+const app = express();
+dotenv.config({ path: './config.env' });
 
 //db
 connectDB();
@@ -16,15 +16,17 @@ app.use(cookieParser());
 app.use(cors({
     origin: 'https://meet-slot-booking-backend-hjain2003.vercel.app',
     credentials: true,
-  }));
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use('/meets', MeetRouter);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send(`Hello world app`);
 });
 
 const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server up and running  at ${PORT}`);
 });
