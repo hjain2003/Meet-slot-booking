3e397b876d2d5fe3de07065ea91da4912acd1468
Harsh <hjain_be21@thapar.edu>
2023-07-16 17:51:56 +0530

    cors again

--- server/app.js
+++ server/app.js
@@ -14,12 +14,12 @@ connectDB();
 
 //middlewares
 app.use(cookieParser());
-// app.use(cors({
-//     origin: 'https://meet-slot-booking.vercel.app',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     // allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
+app.use(cors({
+    origin: 'https://meet-slot-booking.vercel.app',
+    // credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use('/user', userRouter);
 app.use('/meets', MeetRouter);
