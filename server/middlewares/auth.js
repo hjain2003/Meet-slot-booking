39a8e4fbfae5142774915ab75d7cbafac1f83553
Harsh <hjain_be21@thapar.edu>
2023-07-16 23:47:00 +0530

    auth remove

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,29 +1,30 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
-
-export const Authenticate = async(req,res,next)=>{
-    try{
-        const token = req.cookies.jwtoken;
-
-        if (!token) {
-            return res.status(401).send('Unauthorized: No token provided');
-          }
-          
-        const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-
-        const rootUser = await User.findOne({_id : verifyToken._id, "tokens.token":token}); //entire document u can get
-
-        if(!rootUser){
-            throw new Error("User not found");
-        }
-        req.token = token;
-        req.rootUser = rootUser;
-
-        req.userID = rootUser._id;
-        next();
-
-    }catch(err){
+export const Authenticate = async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+  
+      if (!token) {
         return res.status(401).json({ error: 'Unauthorized: No token provided' });
-        console.log(err);
+      }
+  
+      const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+  
+      const rootUser = await User.findOne({
+        _id: verifyToken._id,
+        'tokens.token': token,
+      });
+  
+      if (!rootUser) {
+        throw new Error('User not found');
+      }
+  
+      req.token = token;
+      req.rootUser = rootUser;
+      req.userID = rootUser._id;
+  
+      next();
+    } catch (err) {
+      console.log(err);
+      return res.status(401).json({ error: 'Unauthorized: Invalid token' });
     }
-};
+  };
+  
