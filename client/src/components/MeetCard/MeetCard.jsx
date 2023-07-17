2c6917dd4dcc9d7ef23785fe032c5c2fe3776be6
Harsh <hjain_be21@thapar.edu>
2023-07-17 10:05:40 +0530

    sensitivity

--- client/src/components/MeetCard/MeetCard.jsx
+++ client/src/components/MeetCard/MeetCard.jsx
@@ -57,12 +57,14 @@ const MeetCard = (props) => {
       <div className="card_wrapper">
         <div className="name_buttons">
           <span className="card_info"><b>{props.userName}</b></span>
-          <button id="edit_btn" onClick={handleEdit}>
+          <span onClick={handleEdit}><button id="edit_btn">
             <FaEdit className='edit_del' />
           </button>
-          <button id="delete_btn" className='edit_del' onClick={handleDelete}>
+          </span>
+          <span onClick={handleDelete}><button id="delete_btn" className='edit_del'>
             <FaTrash />
           </button>
+          </span>
         </div>
         <div className="card">
           <span className="card_info"><b>{props.date}</b></span>
